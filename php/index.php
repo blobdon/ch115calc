ac9fc3163268e032b2f2ab512da0059ef3f172e3
Pete <pete.mitchell99@yahoo.com>
2013-07-26 13:03:54 +0100

    switch to local jquery file

--- php/index.php
+++ php/index.php
@@ -133,7 +133,7 @@
   </div> <!-- main container -->
 
 
-   <script src="http://code.jquery.com/jquery.js"></script>
+   <script src="./js/jquery-1.10.2.min.js"></script>
    <script src="./js/bootstrap.min.js"></script>
    <script type="text/javascript" src="./js/bootstrap-datepicker.js"></script>
    <script type="text/javascript" src="./js/date.js"></script>
