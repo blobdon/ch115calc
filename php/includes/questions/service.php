9bad1c08943aae517f5ece406f8cf5331db31ff5
Pete <pete.mitchell99@yahoo.com>
2013-07-30 17:44:43 +0100

    create individual question files, jsut service portion for testing

---
+++ php/includes/questions/service.php
@@ -0,0 +1,41 @@
+<h4 class=''>Please indicate your branch of service, dates of service, and which type of discharge you received.</h4>
+<div class='row'>
+  <div class='span2 <?php echo !empty($_SESSION['errors']['branch']) ? 'question alert-error' : '';?>'>
+    <label class='text-center' for="branch">Branch of service <br>
+    <select class='span2' name='branch' id='branch'>
+      <option value=""></option>
+      <option value="Army"<?php retain_Select('branch','Army');?>>Army</option>
+      <option value="Navy"<?php retain_Select('branch','Navy');?>>Navy</option>
+      <option value="Marines"<?php retain_Select('branch','Marines');?>>Marine Corps</option>
+      <option value="Air Force"<?php retain_Select('branch','Air Force');?>>Air Force</option>
+      <option value="Active Guard/Reserve"<?php retain_Select('branch','Active Guard/Reserve');?>>Active Guard/Reserve</option>
+      <option value="Coast Guard"<?php retain_Select('branch','Coast Guard');?>>Coast Guard</option>
+    </select></label>
+  </div>
+  <div class='span2 <?php echo !empty($_SESSION['errors']['serviceStart']) ? 'question alert-error' : '';?>'>
+    <label class='text-center' for="serviceStart">Date service began <br>
+    <input type="text" class="datepicker span2" placeholder="mm/dd/yy" name='serviceStart' id='serviceStart'
+    value='<?php echo isset($_SESSION['serviceStart'])? htmlspecialchars($_SESSION['serviceStart']):'';?>'>
+    <span><?php echo isset($_SESSION['errors']['serviceStart']) ?$_SESSION['errors']['serviceStart']:'';?></span>
+    </label>
+  </div>
+  <div class='span2 <?php echo !empty($_SESSION['errors']['serviceEnd']) ? 'question alert-error' : '';?>'>
+    <label class='text-center' for="serviceEnd">Date service ended <br>
+    <input type="text" class="datepicker span2" placeholder="mm/dd/yy" name='serviceEnd' id='serviceEnd'
+    value='<?php echo isset($_SESSION['serviceEnd'])? htmlspecialchars($_SESSION['serviceEnd']):'';?>'>
+    <span><?php echo isset($_SESSION['errors']['serviceEnd'])?$_SESSION['errors']['serviceEnd']:'';?></span>
+    </label>
+  </div>
+  <div class='span2 <?php echo !empty($_SESSION['errors']['discharge']) ? 'question alert-error' : '';?>'>
+    <label class='text-center' for="discharge">Type of discharge <br>
+    <select class='span2' name="discharge" id="discharge">
+      <option value=""></option>
+      <option value="Honorable"<?php retain_Select('discharge','Honorable');?>>Honorable</option>
+      <option value="General"<?php retain_Select('discharge','General');?>>General</option>
+      <option value="OTH"<?php retain_Select('discharge','OTH');?>>Other than Honorable</option>
+      <option value="Dishonorable"<?php retain_Select('discharge','Dishonorable');?>>Dishonorable</option>
+      <option value="Bad Conduct"<?php retain_Select('discharge','Bad Conduct');?>>Bad Conduct</option>
+      <option value="Other"<?php retain_Select('discharge','Other');?>>Other</option>
+    </select></label>
+  </div>
+</div>
---
+++ php/includes/questions/serviceDeath.php
@@ -0,0 +1,14 @@
+<h4>Did the veteran die while on active duty? (under conditions other than dishonorable)</h4>
+<div class='span8'>
+  <input type='radio' name='serviceDeath' id='serviceDeathInclude' value='Skipped' class='hide' checked>
+  <label class="radio inline" for='serviceDeath1'>
+    <input type='radio' name='serviceDeath' id='serviceDeath1' value='Yes'
+    <?php retain_Radio('serviceDeath','Yes');?>>
+    YES
+  </label>
+  <label class="radio inline" for='serviceDeath0'>
+    <input type='radio' name='serviceDeath' id='serviceDeath0' value='No'
+    <?php retain_Radio('serviceDeath','No');?>>
+    NO
+   </label>
+</div>
