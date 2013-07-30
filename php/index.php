1b5eecde3b1da0857258db31201c31d8943b5ee3
Pete <pete.mitchell99@yahoo.com>
2013-07-30 18:11:42 +0100

    add questions array

--- php/index.php
+++ php/index.php
@@ -16,7 +16,19 @@
       );
     $_SESSION['index'] = 0;
   }
+  if ( !isset($_SESSION['questions']) ){
+    $_SESSION['questions'] = array(
+      'service' => array('status' => 'none', 'filename' => 'service.php'),
+      'kdsm' => array('status' => 'none', 'filename' => 'kdsm.php'),
+      'campaigns' => array('status' => 'none', 'filename' => 'campaigns.php'),
+      'purpleHeart' => array('status' => 'none', 'filename' => 'purpleHeart.php'),
+      'serviceDisablility' => array('status' => 'none', 'filename' => 'serviceDisablility.php'),
+      'serviceDeath' => array('status' => 'none', 'filename' => 'serviceDeath.php')
+      );
+    $_SESSION['index'] = 0;
+  }
   require_once(dirname(__FILE__).'/includes/myFunctions.php');
+
 ?>
 
 <html lang='en'>
