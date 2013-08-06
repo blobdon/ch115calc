38deda8ac430e5bf69b4d3c0e35fab31f7e05b2e
Pete <pete.mitchell99@yahoo.com>
2013-08-06 18:08:28 +0100

    update intro page

--- php/includes/intro.php
+++ php/includes/intro.php
@@ -1,25 +1,71 @@
 <div class='row'>
-<div class='span8'>
-	<h4>Welcome to the Chapter 115 eligibility calcuator!</h4>
-	<p>Some Instructions</p>
-	<strong>Information you will need:</strong>
-	<ul>
-		<li>Service Dates</li>
-		<li>Income</li>
-		<li>Housing Expenses</li>
-	</ul>
-	<p>
-		Please complete all of the questions to determine your eligbility for Chapter 115 financial assistance.
-		There are many ways to be eligible for this benefit, so please do not make any assumptions about your eligibility based on any of the question you are asked. Your elgibility may not be affected by every question, but you won't know unless you complete all of them.
-	</p>
-	<p>
-		Please keep in mind that the results of our calculator are only an estimate, a final determination of your benefits can only be made by the Department of Veteran's Services. We highly encourage you to apply through your local Veterans' Service Officer (VSO).
-	</p>
-	<p>
-		If you have previously applid for this benefit through your local VSO, but feel that you did not receive the correct determination from them, remember that you have the right to appeal the decision. We are available to aid you in your appeal.
-	</p>
-	<p>
-		Please contact us if you have any questions about this form or your veteran's benefits generally.
-	</p>
+	<p>By asking you a series of questions, this tool will estimate your eligibility for Massachusetts Chapter 115 Financial and Medical Assistance.</p>
+	<div class='span3 well'>
+		<div class='row'>
+			<div class='span3 text-center'><h4>About this calculator</h4></div>
+		</div>
+		<div class='row'>
+			<div class='span3'>
+			<b>Information you will need:</b>
+			<ul>
+				<li>Veteran's Active-Duty Service Dates,</li>
+				<li>Your Income,</li>
+				<li>Your Housing Expenses</li>
+			</ul>
+			</div>
+		</div>
+		<div class='row'>
+			<div class='span3'>
+			<b>How long will it take?</b>
+			<p>Less than 10 minutes. Usually less than 20 questions. Your answers are completely anonymous.</p>
+			</div>
+		</div>
+	</div>
+	<div class='span3 well'>
+		<div class='row'>
+			<div class='span3 text-center'><h4>Important Notes</h4></div>
+		</div>
+		<div class='row'>
+			<div class='span3'>
+			<b>This is only an estimate.</b> 
+			<p>A final determination of your benefits can only be made by the Department of Veteran's Services. We highly encourage you to apply through your local Veterans' Service Officer (VSO).</p>
+			</div>
+		</div>
+		<div class='row'>
+			<div class='span3'>
+			<b>Questions?</b>
+			<p>Contact us</p>
+			</div>
+		</div>
+	</div>
+	<div class='span3 well'>
+		<div class='row'>
+				<div class='span3 text-center'><h4>How to use the calculator</h4></div>
+		</div>
+		<div class='row'>
+			<div class='span3'>
+				<ul>
+					<li>Press <button disabled class='btn btn-primary btn-mini'>Continue</button> to submit your current answer</li>
+					<li>Press <button disabled class='btn btn-mini btn-link'>Edit</button> to change a previous answer.</p></li>
+				</ul> 
+			</div>
+		</div>
+		<div class='row'>
+			<div class='span3'>
+				<b>Complete all of the questions.</b>
+				<p>There are many ways to be eligible, so please do not make any assumptions based on any question. You won't know unless you answer them all.</p>
+			</div>
+		</div>
+		
+			
+	</div>
+
+
 </div>
+<div class='row'>
+
+</div>
+<div class='row'>
+	
+
 </div>
